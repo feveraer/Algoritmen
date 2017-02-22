ca23b0802a6e3b2fcf66f7b88ed48a29f0c9b0cb
feveraer <frederic.everaert@gmail.com>
2017-02-22 09:23:33 +0100

    labo 2 init

---
+++ Labo2/main.cpp
@@ -0,0 +1,23 @@
+/*
+
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Frederic
+ *
+ * Created on 22 februari 2017, 9:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Labo2/sortvector.h
@@ -0,0 +1,67 @@
+#ifndef __SORTVECTOR
+#define __SORTVECTOR
+/**
+ \class sortvector
+ \brief is een klasse van sorteerbare vectoren
+ Bovenop de vectoreigenschappen zijn er hulpfuncties om sorteervergelijkingen
+ te vergemakkelijken.
+*/
+#include <iostream>
+   using std::istream;
+   using std::ostream;
+    using std::move;
+    using std::swap;
+#include <iomanip>   // voor setw
+#include <cstdlib>   // voor rand - opletten!! 
+#include <vector>
+    using std::vector;
+
+template<class T>
+class Sortvector:public vector<T>{
+  public:
+
+    /// \fn Constructor: het argument geeft de grootte aan
+    /// bij constructie zal de tabel opgevuld worden met
+    /// n verschillende Ts in random volgorde
+    /// (zie hulplidfuncties)
+    Sortvector(int);
+    
+    Sortvector(const Sortvector<T>& v) = delete;
+    Sortvector<T>& operator=(const Sortvector<T>& v) = delete;
+    Sortvector<T>& operator=(Sortvector<T>&& v) = delete;
+    Sortvector(Sortvector<T>&& v) = delete;
+
+    /// \fn vul_range vul vector met de waarden T(0)...T(size()-1) in volgorde
+    void vul_range();
+    void draai_om();
+    void vul_omgekeerd();
+    void shuffle();
+    void vul_random_zonder_dubbels();
+    void vul_random(); //< nog niet implementeren
+    
+    bool is_gesorteerd() const;
+    /// \fn is_range controleert of *this eruit ziet als het resultaat van vul_range(), d.w.z.
+    /// dat, voor alle i, (*this)[i]==T(i);
+    bool is_range() const;
+
+    friend ostream& operator<<(ostream& os, const Sortvector<T>& s){
+        s.schrijf(os);
+        return os;
+    }
+
+  private: 
+    void schrijf(ostream & os)const;
+    
+};
+
+template <class T>
+void Sortvector<T>::schrijf(ostream & os)const{
+    for(auto&& t : *this){
+        os<<t<<" ";
+    }
+    os<<"\n";
+}
+
+
+
+#endif
