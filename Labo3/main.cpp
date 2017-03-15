1306a43ceaa039b9fa9bc5ccf86e25aff4408459
feveraer <frederic.everaert@gmail.com>
2017-03-15 11:06:47 +0100

    Labo3: hou gesorteerde woord bij als key, effectieve woord als value

--- Labo3/main.cpp
+++ Labo3/main.cpp
@@ -18,11 +18,6 @@
 
 using namespace std;
 
-bool sort_by_sec(const pair<string, string> &a,
-        const pair<string, string> &b) {
-    return (a.second < b.second);
-}
-
 int main(int argc, char** argv) {
     ifstream in("woordenlijst.txt");
     string word;
@@ -33,33 +28,33 @@ int main(int argc, char** argv) {
         // copy word before sorting
         string sorted_word = word;
         sort(sorted_word.begin(), sorted_word.end());
-        anagram_pairs.emplace_back(move(word), move(sorted_word));
+        anagram_pairs.emplace_back(move(sorted_word), move(word));
     }
 
-    sort(anagram_pairs.begin(), anagram_pairs.end(), sort_by_sec);
+    sort(anagram_pairs.begin(), anagram_pairs.end());
 
     int number_groups_anagrams = 1;
     int max_number_groups_anagrams = number_groups_anagrams;
-    string s_anagram = anagram_pairs[0].second;
+    string s_anagram = anagram_pairs[0].first;
     int pos_max_number_groups_anagrams;
     
     int pos_largest_anagram;
     int max_size_anagram = 0;
     for (int i = 1; i < anagram_pairs.size(); i++) {
-        if (anagram_pairs[i].second == s_anagram) {
+        if (anagram_pairs[i].first == s_anagram) {
             number_groups_anagrams++;
             if (number_groups_anagrams > max_number_groups_anagrams) {
                 max_number_groups_anagrams = number_groups_anagrams;
                 pos_max_number_groups_anagrams = i - max_number_groups_anagrams + 1;
             }
-            int size_anagram = anagram_pairs[i].second.size();
+            int size_anagram = anagram_pairs[i].first.size();
             if (size_anagram > max_size_anagram) {
                 max_size_anagram = size_anagram;
                 pos_largest_anagram = i;
             }
             
         } else {
-            s_anagram = anagram_pairs[i].second;
+            s_anagram = anagram_pairs[i].first;
             number_groups_anagrams = 1;
         }
     }
@@ -77,7 +72,7 @@ int main(int argc, char** argv) {
     cout << "Largest anagram\n";
     
     cout << max_size_anagram << "\n";
-    cout << anagram_pairs[pos_largest_anagram].second << "\n";
+    cout << anagram_pairs[pos_largest_anagram].first << "\n";
 
     return 0;
 }
